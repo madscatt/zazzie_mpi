b5c7585c757734e680cbd4db6f0eb257584034d7
Joseph E. Curtis <madscatt@gmail.com>
2017-06-21 13:44:30 -0400

    mpich running on external hosts correctly

--- mpi_basics/template.cpp
+++ mpi_basics/template.cpp
@@ -2,38 +2,50 @@
 #include <string.h>
 #include "mpi.h"
 
+#include <time.h>
+ 
+void sleep(unsigned int mseconds)
+{
+    clock_t goal = mseconds + clock();
+    while (goal > clock());
+}
+
 int main(int argc, char* argv[]){
 	int my_rank;
 	int p;
 	int source;
 	int dest;
 	int tag = 0;
- 	char message[100];
+	char message[100];
 	MPI_Status status;
+    char processor_name[MPI_MAX_PROCESSOR_NAME] ;
+    int name_length ;
 
-    printf("hello, I am running and ready for mpi stuff\n") ;
-        
-	MPI_Init(&argc, &argv);
+//    printf("hello, I am running and ready for mpi stuff\n") ;
 
-	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+    MPI_Init(&argc, &argv);
 
 	MPI_Comm_size(MPI_COMM_WORLD, &p);
+	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
-	if(my_rank!=0){
+    MPI_Get_processor_name(processor_name , &name_length) ;
 
-		sprintf(message, "Hello from process %d!", my_rank);
-		dest = 0;
+    printf("my_rank = %d\t p = %d\n", my_rank, p) ;
+    printf("processor_name = %s\n", processor_name) ;
 
-		MPI_Send(message, strlen(message) + 1, MPI_CHAR, dest, tag, MPI_COMM_WORLD);
+    if(my_rank != 0){
+	    sprintf(message, "Hello from process %d!", my_rank);
+	    dest = 0;
+	    MPI_Send(message, strlen(message) + 1, MPI_CHAR, dest, tag, MPI_COMM_WORLD);
+    }
+    else{
+	    for(source = 1; source < p; source ++) {
+	        MPI_Recv(message, 100, MPI_CHAR, source, tag, MPI_COMM_WORLD, &status);
+	        printf("%s\n", message);
+	} ; 
+	} ; 
 
-	} else {
-		for(source = 1; source < p; source ++) {
-			MPI_Recv(message, 100, MPI_CHAR, source, tag, MPI_COMM_WORLD, &status);
-			printf("%s\n", message);
-		}
-	}
-
-	MPI_Finalize;
+	MPI_Finalize();
 }
 
 
