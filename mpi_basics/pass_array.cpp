a0efaa69148f29b60774c5a79d1fb9dec34d3b3a
Joseph E. Curtis <madscatt@gmail.com>
2017-07-21 22:51:24 -0400

    mac os mpi support ; verified send/recv of rows

--- mpi_basics/pass_array.cpp
+++ mpi_basics/pass_array.cpp
@@ -21,25 +21,10 @@ int main(int argc, char* argv[]){
     char processor_name[MPI_MAX_PROCESSOR_NAME] ;
     int name_length ;
 
-    int nframes = 10 ; 
+    int nframes = 4 ; 
     int natoms = 10 ; 
     float x[nframes][natoms] ;
 
-    int count = 1 ;
-    for(int i=0 ; i < nframes ; i++){
-        for(int j = 0 ; j < natoms ; j++){
-            x[i][j] = float(count) ;
-            count++ ;
-        }
-    }
-
-    printf("%s\n", "outside mpi") ;
-	printf("x[0][0] = %f\n", x[0][0]);
-	printf("x[0][1] = %f\n", x[0][1]);
-	printf("x[0][8] = %f\n", x[0][8]);
-	printf("x[0][9] = %f\n", x[0][9]);
-	printf("x[9][8] = %f\n", x[9][8]);
-	printf("x[9][9] = %f\n", x[9][9]);
 
     MPI_Init(&argc, &argv);
 
@@ -48,23 +33,42 @@ int main(int argc, char* argv[]){
 
     MPI_Get_processor_name(processor_name , &name_length) ;
 
-    if(my_rank != 0){
-        sprintf(message, "Hello from process rank = %d : running on %s", my_rank, processor_name);
-	    dest = 0;
-	    MPI_Send(message, strlen(message) + 1, MPI_CHAR, dest, tag, MPI_COMM_WORLD);
-        MPI_Send(&(x[my_rank*2][0]), natoms, MPI_FLOAT, dest, 0, MPI_COMM_WORLD) ;
+    if(my_rank == 0){
+        for(int i=0 ; i < nframes ; i++){
+            for(int j = 0 ; j < natoms ; j++){
+                x[i][j] = float(i*10) + float(j) ;
+            }
+        }
+        printf("%s\n", "rank zero showing typical values") ;
+	    printf("x[0][0] = %f\n", x[0][0]);
+	    printf("x[0][1] = %f\n\n", x[0][1]);
+	    printf("x[2][0] = %f\n\n", x[2][0]);
+	    printf("x[0][8] = %f\n", x[0][8]);
+	    printf("x[0][9] = %f\n", x[0][9]);
+	    printf("x[9][8] = %f\n", x[9][8]);
+	    printf("x[9][9] = %f\n", x[9][9]);
+    }
+
+    if(my_rank == 0){
+        MPI_Send(&(x[2][0]), 20, MPI_FLOAT, 1, 0, MPI_COMM_WORLD) ;
     }
     else{
-	    for(source = 1; source < number_of_processes; source ++) {
-	        MPI_Recv(message, 100, MPI_CHAR, source, tag, MPI_COMM_WORLD, &status);
-	        MPI_Recv(&(x[0][0]), natoms, MPI_FLOAT, source, 0, MPI_COMM_WORLD, &status) ;
-	        printf("%s\n", message);
-	        printf("x[0][0] = %f\n", x[0][0]);
-	        printf("x[0][1] = %f\n", x[0][1]);
-	        printf("x[0][8] = %f\n", x[0][8]);
-	        printf("x[0][9] = %f\n", x[0][9]);
-        };
-	} ; 
+        MPI_Recv(&(x[0][0]), 20, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, &status) ;
+        
+	    printf("my_rank = %i\tx[0][0] = %f\n", my_rank, x[0][0]);
+	    printf("my_rank = %i\tx[0][1] = %f\n", my_rank, x[0][1]);
+	    printf("my_rank = %i\tx[0][2] = %f\n", my_rank, x[0][2]);
+	    printf("my_rank = %i\tx[0][3] = %f\n", my_rank, x[0][3]);
+	    printf("my_rank = %i\tx[1][0] = %f\n", my_rank, x[1][0]);
+	    printf("my_rank = %i\tx[1][1] = %f\n", my_rank, x[1][1]);
+	    printf("my_rank = %i\tx[1][2] = %f\n", my_rank, x[1][2]);
+	    printf("my_rank = %i\tx[1][3] = %f\n", my_rank, x[1][3]);
+	    printf("my_rank = %i\tx[2][0] = %f\n", my_rank, x[2][0]);
+	    printf("my_rank = %i\tx[2][1] = %f\n", my_rank, x[2][1]);
+	    printf("my_rank = %i\tx[2][2] = %f\n", my_rank, x[2][2]);
+	    printf("my_rank = %i\tx[2][3] = %f\n", my_rank, x[2][3]);
+    }
+
 
 	MPI_Finalize();
 }
